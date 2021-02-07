c652bff5b376df9a9f9bc737edbe09a84040d8e4
Maksimova Marina <mmaksimova@luxoft.com>
2021-02-07 20:19:48 +0100

    Added options for avoiding Chrome crashes in container

--- test_cases_automated/conftest.py
+++ test_cases_automated/conftest.py
@@ -4,6 +4,15 @@ from selenium import webdriver
 
 @pytest.fixture(scope="session")
 def browser():
-    driver = webdriver.Chrome()
+    # this options are needed for avoiding crashes inside docker container
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--window-size=1420,1080')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     yield driver
     driver.quit()
+
+
